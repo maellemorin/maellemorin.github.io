804a93fc689d6ba6583fe02588f43772cb41a366
Maëlle Morin <MaelleMorin@MacBook-Pro-de-Maelle.local>
2017-09-15 17:03:53 -0400

    update 15 sept

--- js/script.js
+++ js/script.js
@@ -7,11 +7,22 @@ $(document).ready(function() {
         });
 
 
-      $(".iframe").colorbox({iframe:true, width:"80%", height:"80%", opacity: 0.85});
-$("#graphic1").colorbox({width:"80%", height:"80%"});
-        $(document).scroll(function () {
-      console.log("hello");
-       //$("#navbar").css("background-color", "rgba(45,58,88,0.5)");
-      //  $("#navbar").css("position", "absolute")
-    })
+// $("#graphic1").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
+// $("#graphic2").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
+
+$(".graphic").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
+$(".prog").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
+
+//disbale scrolling when colorbox is opened
+$(document).on('cbox_open', function() {
+  $.fn.fullpage.setAllowScrolling(false);
+  $.fn.fullpage.setKeyboardScrolling(false);
+});
+
+//enable scrolling when colorbox is closed
+$(document).on('cbox_closed', function() {
+  $.fn.fullpage.setAllowScrolling(true);
+  $.fn.fullpage.setKeyboardScrolling(true);
+});
+
 });
