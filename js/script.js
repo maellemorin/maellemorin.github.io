5d9a8070125168de7e6b40e7503698dd6705d0bc
Maelle Morin <guoxi451@hotmail.com>
2018-08-22 23:09:17 -0400

    index and js

--- js/script.js
+++ js/script.js
@@ -4,17 +4,20 @@ $(document).ready(function() {
         controlArrows: false,
         slidesNavigation: true,
         normalScrollElements: '#gallery'
-        });
+        // scrollOverflow: true
 
 
+});
 // $("#graphic1").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
 // $("#graphic2").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
 
-$(".graphic").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
-$(".prog").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
-$(".D").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
-$(".vid").colorbox({width:"90%", height:"90%", opacity: 0.8, retinaImage:true, retinaUrl:true});
-$(".photo1").colorbox({width:"90%", height:"90%", opacity: 0.8, rel:"photo1"});
+$(".graphic").colorbox({width:"98%", height:"98%", opacity: 0.9, retinaImage:true, retinaUrl:true});
+$(".prog").colorbox({width:"98%", height:"98%", opacity: 0.9, retinaImage:true, retinaUrl:true});
+$(".D").colorbox({width:"98%", height:"98%", opacity: 0.9, retinaImage:true, retinaUrl:true});
+$(".vid").colorbox({width:"98%", height:"98%", opacity: 0.9, retinaImage:true, retinaUrl:true});
+$(".photo1").colorbox({width:"98%", height:"98%", opacity: 0.9, rel:"photo1"});
+
+
 //disbale scrolling when colorbox is opened
 $(document).on('cbox_open', function() {
   $.fn.fullpage.setAllowScrolling(false);
